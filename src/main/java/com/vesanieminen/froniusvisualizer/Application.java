27eaa120175425db1fffb2edef7306940890227f
start.vaadin.com <no-reply@vaadin.com>
2022-09-25 06:02:35 +0000

    Generated project

---
+++ src/main/java/com/vesanieminen/froniusvisualizer/Application.java
@@ -0,0 +1,27 @@
+package com.vesanieminen.froniusvisualizer;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@Theme(value = "froniusvizualizer")
+@PWA(name = "FroniusVizualizer", shortName = "FroniusVizualizer", offlineResources = {})
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class Application implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
