9e9fc25f746554656a2f75548c6a0d1dab60983e
Vesa Nieminen <vesa@vaadin.com>
2022-10-04 23:02:09 +0300

    Added week's worth of data from Nordpoolspot

--- src/main/java/com/vesanieminen/froniusvisualizer/views/NordpoolspotView.java
+++ src/main/java/com/vesanieminen/froniusvisualizer/views/NordpoolspotView.java
@@ -4,11 +4,14 @@ import com.fatboyindustrial.gsonjavatime.Converters;
 import com.google.gson.GsonBuilder;
 import com.vaadin.flow.component.charts.Chart;
 import com.vaadin.flow.component.charts.model.ChartType;
-import com.vaadin.flow.component.charts.model.Label;
-import com.vaadin.flow.component.charts.model.ListSeries;
+import com.vaadin.flow.component.charts.model.DataSeries;
+import com.vaadin.flow.component.charts.model.DataSeriesItem;
 import com.vaadin.flow.component.charts.model.Marker;
 import com.vaadin.flow.component.charts.model.PlotLine;
 import com.vaadin.flow.component.charts.model.PlotOptionsLine;
+import com.vaadin.flow.component.charts.model.RangeSelector;
+import com.vaadin.flow.component.charts.model.RangeSelectorButton;
+import com.vaadin.flow.component.charts.model.RangeSelectorTimespan;
 import com.vaadin.flow.component.charts.model.Tooltip;
 import com.vaadin.flow.component.charts.model.XAxis;
 import com.vaadin.flow.component.charts.model.YAxis;
@@ -30,65 +33,93 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
-import java.time.format.FormatStyle;
 import java.util.Locale;
 import java.util.function.Function;
-import java.util.stream.IntStream;
 
 @Route("")
 public class NordpoolspotView extends Div {
 
     private final DecimalFormat df = new DecimalFormat("#.00");
+
     public NordpoolspotView() throws URISyntaxException, IOException, InterruptedException, ParseException {
         addClassNames(LumoUtility.Display.FLEX, LumoUtility.FlexDirection.COLUMN, LumoUtility.AlignItems.CENTER, LumoUtility.TextColor.PRIMARY_CONTRAST);
         setHeightFull();
 
-        final var request = HttpRequest.newBuilder().uri(new URI("https://www.nordpoolgroup.com/api/marketdata/page/10")).GET().build();
-        final var response = HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString());
+        final var request = HttpRequest.newBuilder().uri(new URI("https://www.nordpoolspot.com/api/marketdata/page/35?currency=,,EUR,EUR")).GET().build();
+        final var response = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL).build().send(request, HttpResponse.BodyHandlers.ofString());
         final var gson = Converters.registerAll(new GsonBuilder()).create();
         var nordpoolResponse = gson.fromJson(response.body(), NordpoolResponse.class);
         //Stream.of(nordpoolResponse.data.getClass().getDeclaredFields()).forEach(field -> getAdd(nordpoolResponse, field));
 
         var chart = new Chart(ChartType.LINE);
+        chart.setTimeline(true);
         chart.getConfiguration().getChart().setStyledMode(true);
         chart.setHeightFull();
         add(chart);
 
+
         NumberFormat format = NumberFormat.getInstance(Locale.FRANCE);
-        format.setMaximumFractionDigits(2);
-        final var prices = nordpoolResponse.data.Rows.subList(0, 24).stream().map(getRowNumberFunction(format)).toList();
-        final var dateTimeFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT);
-        final var listSeries = new ListSeries("Electricity price on " + nordpoolResponse.data.DataStartdate.format(dateTimeFormatter) + " - " + nordpoolResponse.data.DataEnddate.format(dateTimeFormatter), prices);
-        chart.getConfiguration().addSeries(listSeries);
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        final var dataSeries = new DataSeries("FI electricity price");
+        final var rows = nordpoolResponse.data.Rows;
+        int columnIndex = 6;
+        while (columnIndex >= 0) {
+            for (NordpoolResponse.Row row : rows.subList(0, rows.size() - 6)) {
+                final var dataSeriesItem = new DataSeriesItem();
+                final var time = row.StartTime.toString().split("T")[1];
+                NordpoolResponse.Column column = row.Columns.get(columnIndex);
+                final var dateTimeString = column.Name + " " + time;
+                dataSeriesItem.setX(LocalDateTime.parse(dateTimeString, dateTimeFormatter).toInstant(ZoneOffset.UTC));
+                dataSeriesItem.setY(format.parse(column.Value).doubleValue() * 1.24d / 10);
+                dataSeries.add(dataSeriesItem);
+            }
+            --columnIndex;
+        }
+        chart.getConfiguration().addSeries(dataSeries);
+
         final var plotOptionsLine = new PlotOptionsLine();
         plotOptionsLine.setMarker(new Marker(true));
         chart.getConfiguration().setPlotOptions(plotOptionsLine);
         final var tooltip = new Tooltip();
-        tooltip.setFormatter("function() { return this.y + ' c/kWh <br/>' + this.x + ' - ' + (parseInt(this.x.split(':')[0]) + 1) + ':00' }");
+        tooltip.setValueDecimals(2);
+        tooltip.setUseHTML(true);
+        tooltip.setXDateFormat("%A<br />%H:%M %e.%m.%Y");
+        tooltip.setPointFormat("{point.y} c/kWh");
         chart.getConfiguration().setTooltip(tooltip);
 
         final var xAxis = new XAxis();
-        xAxis.setCategories(IntStream.range(0, 24).mapToObj(i -> i + ":00").toArray(String[]::new));
+        //xAxis.setCategories(IntStream.range(0, 24).mapToObj(i -> i + ":00").toArray(String[]::new));
         xAxis.setTitle("Time");
         chart.getConfiguration().addxAxis(xAxis);
         final var yAxis = new YAxis();
-        yAxis.setMin(0);
+        yAxis.setSoftMin(0);
         yAxis.setTitle("Price");
         chart.getConfiguration().addyAxis(yAxis);
 
-        if (LocalDateTime.now().getDayOfMonth() == nordpoolResponse.data.Rows.get(5).StartTime.getDayOfMonth()) {
-            PlotLine plotLine = new PlotLine();
-            plotLine.setClassName("time");
-            plotLine.setValue(LocalDateTime.now(ZoneId.of("Europe/Helsinki")).getHour());
-            chart.getConfiguration().getxAxis().addPlotLine(plotLine);
-        }
+        //if (LocalDateTime.now().getDayOfMonth() == nordpoolResponse.data.Rows.get(5).StartTime.getDayOfMonth()) {
+        // TODO: the x plotline doesn't work anymore
+        PlotLine plotLine = new PlotLine();
+        plotLine.setClassName("time");
+        plotLine.setValue(LocalDateTime.now(ZoneId.of("Europe/Helsinki")).toEpochSecond(ZoneOffset.UTC));
+        chart.getConfiguration().getxAxis().addPlotLine(plotLine);
+        //}
+        final var rangeSelector = new RangeSelector();
+        rangeSelector.setButtons(
+                new RangeSelectorButton(RangeSelectorTimespan.DAY, 1, "1 day"),
+                new RangeSelectorButton(RangeSelectorTimespan.DAY, 2, "2 days"),
+                new RangeSelectorButton(RangeSelectorTimespan.DAY, 3, "3 days"),
+                new RangeSelectorButton(RangeSelectorTimespan.ALL, "All")
+        );
+        rangeSelector.setSelected(2);
+        chart.getConfiguration().setRangeSelector(rangeSelector);
 
-        final var averageValue = mapToPrice(format, nordpoolResponse.data.Rows.get(26));
-        PlotLine averagePrice = new PlotLine();
-        averagePrice.setLabel(new Label("Average price: " + averageValue + " c/kWh"));
-        averagePrice.setValue(averageValue);
-        chart.getConfiguration().getyAxis().addPlotLine(averagePrice);
+        //final var averageValue = mapToPrice(format, nordpoolResponse.data.Rows.get(26));
+        //PlotLine averagePrice = new PlotLine();
+        //averagePrice.setLabel(new Label("Average price: " + averageValue + " c/kWh"));
+        //averagePrice.setValue(averageValue);
+        //chart.getConfiguration().getyAxis().addPlotLine(averagePrice);
     }
 
     private Function<NordpoolResponse.Row, Number> getRowNumberFunction(NumberFormat format) {
@@ -102,7 +133,7 @@ public class NordpoolspotView extends Div {
     }
 
     private Double mapToPrice(NumberFormat format, NordpoolResponse.Row row) throws ParseException {
-        return Double.valueOf(df.format(format.parse(row.Columns.get(5).Value).doubleValue() * 1.24d / 10));
+        return Double.valueOf(df.format(format.parse(row.Columns.get(5).Value).doubleValue() * 1 / 10));
     }
 
     private void getAdd(NordpoolResponse nordpoolResponse, Field field) {
