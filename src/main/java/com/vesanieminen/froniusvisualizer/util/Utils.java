12b6019a74afed1b6ed85a6886c74b4e7bec3e31
Vesa Nieminen <vesa@vaadin.com>
2022-10-18 22:55:39 +0300

    Tweaking the date information once more.

--- src/main/java/com/vesanieminen/froniusvisualizer/util/Utils.java
+++ src/main/java/com/vesanieminen/froniusvisualizer/util/Utils.java
@@ -15,7 +15,7 @@ public class Utils {
     public static final NumberFormat numberFormat = NumberFormat.getInstance(Locale.FRANCE);
     public static final DecimalFormat decimalFormat = new DecimalFormat("#0.00");
     public static final ZoneId fiZoneID = ZoneId.of("Europe/Helsinki");
-    public static final Locale fiLocale = new Locale("en", "FI");
+    public static final Locale fiLocale = new Locale("fi", "FI");
 
     public static boolean notNull(Object... objects) {
         return Arrays.stream(objects).allMatch(Objects::nonNull);
