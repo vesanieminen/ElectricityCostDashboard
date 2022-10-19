c3485fdda4e7781d91145871db08fa36f2937899
Vesa Nieminen <vesa@vaadin.com>
2022-10-19 14:55:00 +0300

    Moving styles from index.html to styles.css.

---
+++ frontend/src/app-height.js
@@ -0,0 +1,6 @@
+const appHeight = () => {
+    const doc = document.documentElement
+    doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+}
+window.addEventListener('resize', appHeight)
+appHeight()
--- src/main/java/com/vesanieminen/froniusvisualizer/Application.java
+++ src/main/java/com/vesanieminen/froniusvisualizer/Application.java
@@ -29,6 +29,7 @@ import static com.vesanieminen.froniusvisualizer.util.Properties.isStagingEnviro
 @PWA(name = "FroniusVizualizer", shortName = "FroniusVizualizer", offlineResources = {})
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 @JsModule("src/prefers-color-scheme.js")
+//@JsModule("src/app-height.js")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
