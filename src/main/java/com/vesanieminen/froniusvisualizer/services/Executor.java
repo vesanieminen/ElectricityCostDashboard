fc58f77b241d91a87f5f77fd32acda963b0fe649
Vesa Nieminen <vesa@vaadin.com>
2022-11-02 07:01:48 +0200

    Reordered the background data update calls.

--- src/main/java/com/vesanieminen/froniusvisualizer/services/Executor.java
+++ src/main/java/com/vesanieminen/froniusvisualizer/services/Executor.java
@@ -8,9 +8,9 @@ public class Executor {
 
     static {
         ScheduledExecutorService executorService = Executors.newScheduledThreadPool(4);
-        executorService.scheduleAtFixedRate(FingridService::updateRealtimeData, 0, 30, TimeUnit.MINUTES);
-        executorService.scheduleAtFixedRate(FingridService::updateWindEstimateData, 0, 30, TimeUnit.MINUTES);
         executorService.scheduleAtFixedRate(NordpoolSpotService::updateNordpoolData, 0, 30, TimeUnit.MINUTES);
+        executorService.scheduleAtFixedRate(FingridService::updateWindEstimateData, 0, 30, TimeUnit.MINUTES);
+        executorService.scheduleAtFixedRate(FingridService::updateRealtimeData, 0, 30, TimeUnit.MINUTES);
         //executorService.scheduleAtFixedRate(PakastinSpotService::updateData, 0, 30, TimeUnit.MINUTES);
     }
 
