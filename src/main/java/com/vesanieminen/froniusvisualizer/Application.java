6ee10af196f08ce32b97359b27464cfaaece3ed9
Vesa Nieminen <vesa@vaadin.com>
2022-11-26 20:38:38 +0200

    Removed the old Tomcat HTTPS redirect. Cloudflare is now in use so that is not needed anymore.

--- src/main/java/com/vesanieminen/froniusvisualizer/Application.java
+++ src/main/java/com/vesanieminen/froniusvisualizer/Application.java
@@ -6,17 +6,8 @@ import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import com.vesanieminen.froniusvisualizer.services.Executor;
-import org.apache.catalina.Context;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-
-import static com.vesanieminen.froniusvisualizer.util.Properties.isDevelopmentMode;
-import static com.vesanieminen.froniusvisualizer.util.Properties.isStagingEnvironment;
 
 /**
  * The entry point of the Spring Boot application.
@@ -26,7 +17,7 @@ import static com.vesanieminen.froniusvisualizer.util.Properties.isStagingEnviro
  */
 @SpringBootApplication
 @Theme(value = "froniusvizualizer")
-@PWA(name = "Sähkön hinta", shortName = "Sähkön hinta", offlineResources = {})
+@PWA(name = "Liukuri", shortName = "Liukuri", offlineResources = {})
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 @JsModule("src/prefers-color-scheme.js")
 public class Application implements AppShellConfigurator {
@@ -34,26 +25,6 @@ public class Application implements AppShellConfigurator {
     public static void main(String[] args) {
         Executor.init();
         SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        if (isDevelopmentMode() || isStagingEnvironment()) {
-            System.out.println("dev / staging mode");
-            return new TomcatServletWebServerFactory();
-        }
-        System.out.println("production mode");
-        return new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
     }
 
 }
