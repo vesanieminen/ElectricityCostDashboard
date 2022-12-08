cd5559be3f964d1968a9bdccfab4871df2aa1614
Vesa Nieminen <vesa@vaadin.com>
2022-12-08 16:14:43 +0200

    Trying to set the chart zones via plot options. Not working yet.

--- frontend/src/chart-template.ts
+++ frontend/src/chart-template.ts
@@ -7,6 +7,25 @@ import '@vaadin/charts/src/vaadin-chart-series';
 export class ChartTemplate extends LitElement {
 
   static styles = css`
+    .highcharts-point.zone-0 {
+          fill: green;
+    }
+    
+    .highcharts-point.zone-1 {
+          fill: blue;
+    }
+    
+    .highcharts-point.zone-2 {
+          fill: yellow;
+    }
+    
+    .highcharts-point.zone-3 {
+          fill: red;
+    }
+    
+    .highcharts-point.zone-4 {
+          fill: purple;
+    }
   `;
 
   @property()
@@ -37,21 +56,33 @@ export class ChartTemplate extends LitElement {
           type="column"
           categories='["0:00","1:00","2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"]'
           additional-options='{
-              //"legend": {
-              //    layout": "vertical",
-              //    "align": "right",
-              //    "verticalAlign": "middle"
-              //},
-              "xAxis": {
-                  "crosshair": true
-              },
-              "animation": false
-              //"yAxis": {
-              //  "min": -10
-              //}
+            "xAxis": {
+              "crosshair": true
+              "visible":false
+            },
+            "yAxis": {
+              "min": -10,
+            },
+            "plotOptions": {
+                "series": {
+                    "zones": [{
+                        "value": 0,
+                        "className": "zone-0"
+                    }, {
+                        "value": 10,
+                        "className": "zone-1"
+                    }, {
+                        "value": 20,
+                        "className": "zone-2"
+                    }, {
+                        "value": 40,
+                        "className": "zone-3"
+                    }, {
+                        "className": "zone-4"
+                    }]
+                }
+            }, 
             }'
-
-
       >
         <vaadin-chart-series
             title="${this.seriesTitle}"
