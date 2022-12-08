27f9a190b6825b4a68fd3f3c152b0690072711ce
Vesa Nieminen <vesa@vaadin.com>
2022-12-08 21:34:42 +0200

    Removed the yellow color from the charts zones.

--- frontend/src/chart-template.ts
+++ frontend/src/chart-template.ts
@@ -7,89 +7,100 @@ import type {Options} from 'highcharts';
 @customElement('chart-template')
 export class ChartTemplate extends LitElement {
 
-  static styles = css`
+    static styles = css`
   `;
 
-  @property()
-  chartTitle?: string = '';
+    @property()
+    chartTitle?: string = '';
 
-  @property()
-  subtitle?: string = '';
+    @property()
+    subtitle?: string = '';
 
-  @property()
-  seriesTitle?: string = '';
+    @property()
+    seriesTitle?: string = '';
 
-  @property()
-  unit?: string = '';
+    @property()
+    unit?: string = '';
 
-  @property()
-  postfix?: string = 'c/kWh';
+    @property()
+    postfix?: string = 'c/kWh';
 
-  @property()
-  values?: Array<number>;
+    @property()
+    values?: Array<number>;
 
-  private getChartOptions(): Options {
-    const options: Options = {
-      tooltip: {
-        shared: true,
-        //formatter: function() {
-        //  return this.point.name +": <b>" + this.point.y + "</b>";
-        //}
-      },
-      xAxis: {
-        crosshair: true
-      },
-      plotOptions: {
-        column: {
-          borderRadius: 5
-        },
-        series: {
-          zones: [{
-            value: -10,
-            className: "zone-0"
-          }, {
-            value: 20,
-            className: "zone-1"
-          }, {
-            value: 30,
-            className: "zone-2"
-          }, {
-            value: 40,
-            className: "zone-3"
-          }, {
-            className: "zone-4"
-          }]
+    private getChartOptions(): Options {
+        const options: Options = {
+            chart: {
+                type: "column"
+            },
+            tooltip: {
+                shared: true,
+                //formatter: function() {
+                //  return this.point.name +": <b>" + this.point.y + "</b>";
+                //}
+            },
+            xAxis: {
+                crosshair: true
+            },
+            yAxis: {
+                visible: false,
+                plotLines:
+                    [{
+                        color: '#FF0000',
+                        width: 2,
+                        dashStyle: 'LongDashDot',
+                        value: 55
+                    }]
+            },
+            plotOptions: {
+                column: {
+                    borderRadius: 5
+                },
+                series: {
+                    zones: [{
+                        value: -10,
+                        className: "zone-0"
+                    }, {
+                        value: 20,
+                        className: "zone-1"
+                    }, {
+                        value: 30,
+                        className: "zone-2"
+                    }, {
+                        value: 40,
+                        className: "zone-3"
+                    }, {
+                        className: "zone-4"
+                    }]
+                }
+            },
         }
-      },
+        return options;
     }
-    return options;
-  }
 
-  render() {
-    return html`
-      <vaadin-chart
-          style="height:100%"
-          title="${this.chartTitle}"
-          subtitle="${this.subtitle}"
-          tooltip
-          type="column"
-          categories='["0:00","1:00","2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"]'
-          .additionalOptions=${this.getChartOptions()}
-      >
-        <vaadin-chart-series
-            title="${this.seriesTitle}"
-            unit="${this.unit}"
-            .values="${this.values}"
-            additional-options='{
+    render() {
+        return html`
+            <vaadin-chart
+                    style="height:100%"
+                    title="${this.chartTitle}"
+                    subtitle="${this.subtitle}"
+                    categories='["0:00","1:00","2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"]'
+                    .additionalOptions=${this.getChartOptions()}
+            >
+                <vaadin-chart-series
+                        title="${this.seriesTitle}"
+                        unit="${this.unit}"
+                        .values="${this.values}"
+                        additional-options='{
                 "tooltip": {
                     "pointFormat": "{point.y} ${this.postfix}",
                     "valueDecimals": 2
                 },
                 "animation": false
             }'
-        ></vaadin-chart-series>
-      </vaadin-chart>
-    `;
-  }
+                ></vaadin-chart-series>
+            </vaadin-chart>
+        `;
+    }
 
 }
