0b3a8b31957e1f066d3d5cd89302e4877b219ab9
Vesa Nieminen <vesa@vaadin.com>
2022-12-08 21:01:02 +0200

    Moved charts extra configuration from string type to json.

--- frontend/src/chart-template.ts
+++ frontend/src/chart-template.ts
@@ -2,6 +2,7 @@ import {css, html, LitElement} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 import '@vaadin/charts';
 import '@vaadin/charts/src/vaadin-chart-series';
+import type {Options} from 'highcharts';
 
 @customElement('chart-template')
 export class ChartTemplate extends LitElement {
@@ -27,6 +28,41 @@ export class ChartTemplate extends LitElement {
   @property()
   values?: Array<number>;
 
+  private getChartOptions(): Options {
+    const options: Options = {
+      tooltip: {
+        shared: true,
+        //formatter: function() {
+        //  return this.point.name +": <b>" + this.point.y + "</b>";
+        //}
+      },
+      xAxis: {
+        crosshair: true
+      },
+      plotOptions: {
+        series: {
+          zones: [{
+            value: -10,
+            className: "zone-0"
+          }, {
+            value: 20,
+            className: "zone-1"
+          }, {
+            value: 30,
+            className: "zone-2"
+          }, {
+            value: 40,
+            className: "zone-3"
+          }, {
+            className: "zone-4"
+          }]
+        }
+      }
+
+    }
+    return options;
+  }
+
   render() {
     return html`
       <vaadin-chart
@@ -36,33 +72,7 @@ export class ChartTemplate extends LitElement {
           tooltip
           type="column"
           categories='["0:00","1:00","2:00", "3:00", "4:00", "5:00", "6:00", "7:00", "8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"]'
-          additional-options='{
-            "xAxis": {
-              "crosshair": true
-            },
-            "tooltip": {
-                "shared": true
-            },
-            "plotOptions": {
-                "series": {
-                    "zones": [{
-                        "value": -10,
-                        "className": "zone-0"
-                    }, {
-                        "value": 20,
-                        "className": "zone-1"
-                    }, {
-                        "value": 30,
-                        "className": "zone-2"
-                    }, {
-                        "value": 40,
-                        "className": "zone-3"
-                    }, {
-                        "className": "zone-4"
-                    }]
-                }
-            }
-            }'
+          .additionalOptions=${this.getChartOptions()}
       >
         <vaadin-chart-series
             title="${this.seriesTitle}"
