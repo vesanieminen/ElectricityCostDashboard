fa313c7728fc93b9c283f98b314e021f8c50814b
Vesa Nieminen <vesa@vaadin.com>
2022-12-09 12:27:59 +0200

    Updating some of the routes and translations a bit.

--- src/main/java/com/vesanieminen/froniusvisualizer/views/ChartTemplateView.java
+++ src/main/java/com/vesanieminen/froniusvisualizer/views/ChartTemplateView.java
@@ -1,10 +1,14 @@
 package com.vesanieminen.froniusvisualizer.views;
 
 import com.vaadin.flow.component.html.Main;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
 import com.vesanieminen.froniusvisualizer.components.ChartTemplate;
 
-@Route(value = "pylvasgraafi", layout = MainLayout.class)
+@Route(value = "pylvaskaavio", layout = MainLayout.class)
+@RouteAlias(value = "pylvasgraafi", layout = MainLayout.class)
+@PageTitle("column.chart")
 public class ChartTemplateView extends Main {
 
     public ChartTemplateView() {
