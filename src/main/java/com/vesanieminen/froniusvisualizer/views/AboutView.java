86d3d180a3f365c228c10634e58f2813f1ef91f4
Vesa Nieminen <vesa@vaadin.com>
2022-12-18 13:25:13 +0200

    Changed the url for the about page.

--- src/main/java/com/vesanieminen/froniusvisualizer/views/AboutView.java
+++ src/main/java/com/vesanieminen/froniusvisualizer/views/AboutView.java
@@ -7,10 +7,12 @@ import com.vaadin.flow.component.PropertyDescriptors;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
 
 @Tag("about-view")
 @JsModule("src/about-view.ts")
-@Route(value = "about", layout = MainLayout.class)
+@Route(value = "tietoja", layout = MainLayout.class)
+@RouteAlias(value = "about", layout = MainLayout.class)
 public class AboutView extends Component {
 
     private static final PropertyDescriptor<String, String> LANGUAGE = PropertyDescriptors.propertyWithDefault("language", "en");
