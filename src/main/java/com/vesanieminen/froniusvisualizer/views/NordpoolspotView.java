8b44c773e0d64648c5f9b13f537fa3b3ab85a4b2
Vesa Nieminen <vesa@vaadin.com>
2022-12-20 20:57:14 +0200

    Remove the zoom text from the main chart view.

--- src/main/java/com/vesanieminen/froniusvisualizer/views/NordpoolspotView.java
+++ src/main/java/com/vesanieminen/froniusvisualizer/views/NordpoolspotView.java
@@ -167,6 +167,7 @@ public class NordpoolspotView extends Main implements HasUrlParameter<String> {
             lang.setMonths(new String[]{"Tammikuu", "Helmikuu", "Maaliskuu", "Huhtikuu", "Toukokuu", "Kesäkuu", "Heinäkuu", "Elokuu", "Syyskuu", "Lokakuu", "Marraskuu", "Joulukuu"});
             lang.setShortMonths(new String[]{"Tammi", "Helmi", "Maalis", "Huhti", "Touko", "Kesä", "Heinä", "Elo", "Syys", "Loka", "Marras", "Joulu"});
             lang.setWeekdays(new String[]{"Sunnuntai", "Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai"});
+            lang.setRangeSelectorZoom("");
             chartOptions.setLang(lang);
         }
     }
