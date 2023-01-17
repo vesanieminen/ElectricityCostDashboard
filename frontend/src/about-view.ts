426e7f740c6ce626f9a71def96c9ada8e27c8c1f
Serhii Kulykov <iamkulykov@gmail.com>
2023-01-17 10:33:26 +0200

    Add missing space between imports

--- frontend/src/about-view.ts
+++ frontend/src/about-view.ts
@@ -1,5 +1,5 @@
 import {css, html, LitElement, PropertyValues} from 'lit';
-import {customElement,property, state} from 'lit/decorators.js';
+import {customElement, property, state} from 'lit/decorators.js';
 import {registerTranslateConfig, translate, translateUnsafeHTML, use} from "lit-translate";
 
 registerTranslateConfig({
