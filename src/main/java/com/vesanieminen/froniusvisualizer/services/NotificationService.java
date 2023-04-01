1a0db3bd553f083a969bd7a7600eb1ad2dc8536f
Matti Tahvonen <matti@vaadin.com>
2023-04-01 15:10:52 +0300

    first version ready for early bird testing

--- src/main/java/com/vesanieminen/froniusvisualizer/services/NotificationService.java
+++ src/main/java/com/vesanieminen/froniusvisualizer/services/NotificationService.java
@@ -1,13 +1,42 @@
 package com.vesanieminen.froniusvisualizer.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vesanieminen.froniusvisualizer.services.model.NordpoolPrice;
+import com.vesanieminen.froniusvisualizer.services.model.NordpoolResponse;
 import com.vesanieminen.froniusvisualizer.services.model.PriceNotification;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.security.GeneralSecurityException;
+import java.security.Security;
 import java.time.Instant;
-import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import lombok.extern.slf4j.Slf4j;
+import nl.martijndwars.webpush.Notification;
+import nl.martijndwars.webpush.PushService;
+import nl.martijndwars.webpush.Subscription;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.jose4j.lang.JoseException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -22,13 +51,42 @@ public class NotificationService {
     private String privateKey;
     @Value("${vapid.subject}")
     private String subject;
-
+    
     private PushService pushService;
-    
-    
 
     // TODO persist to PostgreSQL with jdbc template before releasing publicly
     List<PriceNotification> notifications = new ArrayList<>();
+    Map<String,Subscription> uidToSubscription = new HashMap<>(); 
+    
+    File uidsubfile = new File("uidsub.json");
+    File notificationsfile = new File("notifications.json");
+    
+    @PostConstruct
+    private void init() throws GeneralSecurityException {
+        try {
+            notifications = mapper.readValue(notificationsfile, new TypeReference<List<PriceNotification>>(){});
+            uidToSubscription = mapper.readValue(uidsubfile, new TypeReference<HashMap<String,Subscription>>(){});
+        } catch( Exception e) {
+            log.info("couln't read old data", e);
+        }
+        
+        Security.addProvider(new BouncyCastleProvider());
+        pushService = new PushService(publicKey, privateKey, subject);
+    }
+    
+    @PreDestroy
+    private void serialize() {
+        try {
+            mapper.writeValue(uidsubfile, uidToSubscription);
+            mapper.writeValue(notificationsfile, notifications);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(NotificationService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(NotificationService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    }
+
 
     public List<PriceNotification> listNotifications(String userId) {
         return new ArrayList<>(notifications.stream().filter(pn -> pn.getUid().equals(userId)).collect(Collectors.toList()));
@@ -36,9 +94,11 @@ public class NotificationService {
 
     public void saveNotifications(String userId, List<PriceNotification> notifications) {
         log.info("Updating notifications for " + userId);
+        Instant now = Instant.now().truncatedTo(ChronoUnit.HOURS);
         
         for (PriceNotification notification : notifications) {
             notification.setUid(userId);
+            notification.setLastTriggered(now);
         }
         clearAll(userId);
         this.notifications.addAll(notifications);
@@ -54,28 +114,88 @@ public class NotificationService {
         }
     }
 
-    @Scheduled(cron = "0 * * * *")
+    @Scheduled(cron = "0 * * * * *")
     public void sendNotifications() {
         log.info("Starting to send hourly notifications...");
 
-        Instant now = Instant.now();
-        Double priceNow = 30.0; // TODO
-        Double previousPrice = 1.0; // TODO
-        boolean up = priceNow > previousPrice;
-        Double lowerBound = up ? previousPrice : priceNow;
-        Double upperBound = up ? priceNow : previousPrice;
-        
-        // TODO add support for pre-warning
+        Instant now = Instant.now().truncatedTo(ChronoUnit.HOURS);
+        Instant nowMinus1Hour = now.minus(1, ChronoUnit.HOURS);
 
+        List<NordpoolPrice> latest7DaysList = NordpoolSpotService.getLatest7DaysList();
+        NordpoolPrice prev = latest7DaysList.stream().filter(p->p.timeInstant().equals(nowMinus1Hour)).findFirst().orElse(null);
+        NordpoolPrice current = latest7DaysList.stream().filter(p->p.timeInstant().equals(now)).findFirst().orElse(null);
+
+        if(prev == null || current == null) {
+            log.info("Couldn't find previous or current price, skipping");
+            return;
+        }
+
+        final Double priceNow = current.price()*getVat(current.timeInstant());
+        final Double previousPrice = prev.price()*getVat(prev.timeInstant());
+        final boolean up = priceNow > previousPrice;
+        final Double lowerBound = up ? previousPrice : priceNow;
+        final Double upperBound = up ? priceNow : previousPrice;
+        Set<PriceNotification> uidsToClean = new HashSet<>();
+        
         notifications.stream()
-                .filter(PriceNotification::isEnabled)
+               .filter(PriceNotification::isEnabled)
+                .filter(pn -> pn.getLastTriggered().plus(pn.getTimeout(),ChronoUnit.HOURS).isBefore(now))
                 .filter(pn -> pn.isUp() == up)
                 .filter(pn -> pn.getPrice() >= lowerBound && pn.getPrice() <= upperBound )
                 .forEach(pn -> {
-                    // TODO send all notifications
-                    
+                    String body =
+                            "Price now %s c/kWh. %s".formatted(priceNow, pn.getExtraMsg());
+                    String title = up ? "Prices going up!": "Prices going down!";
+                    Message message = new Message(title, body);
+                    Subscription s = uidToSubscription.get(pn.getUid());
+                    if(s != null) {
+                       sendNotification(s, message);
+                       pn.setLastTriggered(now);
+                    } else {
+                        uidsToClean.add(pn);
+                    }
                 });
-        log.info("All notifications sent...");
+        notifications.removeAll(uidsToClean);
+        log.info("All notifications  sent...");
     }
 
+    private double getVat(Instant timeInstant) {
+        return timeInstant.isBefore(Instant.parse("2023-05-01T03:00:00Z")) ? 1.10 : 1.24;
+    }
+
+    void sendNotification(Subscription subscription, Message msg) {
+        log.info("sendNotification: " + subscription.endpoint);
+        try {
+            HttpResponse response = pushService.send(new Notification(subscription, mapper.writeValueAsString(msg)));
+            int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode != 201) {
+                System.out.println("Server error, status code:" + statusCode);
+                InputStream content = response.getEntity().getContent();
+                List<String> strings = IOUtils.readLines(content, "UTF-8");
+                System.out.println(strings);
+            }
+        } catch (GeneralSecurityException | IOException | JoseException | ExecutionException
+                 | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void subscribe(String uid, Subscription subscription) {
+        uidToSubscription.put(uid, subscription);
+    }
+
+    public void unsubscribe(String uid) {
+        uidToSubscription.remove(uid);
+    }
+
+    public String getPublicKey() {
+        return publicKey;
+    }
+
+    public record Message(String title, String body) {
+        
+    }
+
+    ObjectMapper mapper = new ObjectMapper();
+    
 }
--- src/main/java/com/vesanieminen/froniusvisualizer/views/NotificationsView.java
+++ src/main/java/com/vesanieminen/froniusvisualizer/views/NotificationsView.java
@@ -1,19 +1,26 @@
 package com.vesanieminen.froniusvisualizer.views;
 
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.DomEvent;
+import com.vaadin.flow.component.EventData;
+import com.vaadin.flow.component.UI;
 import com.vesanieminen.froniusvisualizer.services.model.PriceNotification;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.checkbox.Checkbox;
 
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Paragraph;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.NumberField;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.shared.Registration;
 import com.vesanieminen.froniusvisualizer.services.NotificationService;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import nl.martijndwars.webpush.Subscription;
+import org.vaadin.firitin.components.button.DeleteButton;
 import org.vaadin.firitin.components.select.VSelect;
 import org.vaadin.firitin.components.textfield.VIntegerField;
 import org.vaadin.firitin.components.textfield.VNumberField;
@@ -28,7 +35,6 @@ public class NotificationsView extends VerticalLayout {
     private final NotificationService service;
 
     public static class PriceNotificationEditor {
-
         Checkbox enabled = new Checkbox();
         NumberField price = new VNumberField().withWidth("5em");
         VSelect<Boolean> up = new VSelect<Boolean>()
@@ -38,39 +44,45 @@ public class NotificationsView extends VerticalLayout {
         // VIntegerField prewarn = new VIntegerField().withWidth("5em");
         VIntegerField timeout = new VIntegerField().withWidth("5em");
         VTextField extraMsg = new VTextField();
-
     }
 
     private List<PriceNotification> notifications;
+    
+    Button requestNotificationsBtn;
 
     public NotificationsView(NotificationService service) {
         this.service = service;
         add(new H1("Notifications"));
-
-        WebStorage.getItem("uid", uid -> {
-            if (uid == null) {
-                uid = UUID.randomUUID().toString();
-                WebStorage.setItem("uid", uid);
-                notifications = new ArrayList<>();
-            } else {
-                notifications = service.listNotifications(uid);
-            }
-            this.uid = uid;
-            bindData();
-        });
-
         add(new Paragraph("""
 Tällä näytöllä voit konfiguraoida itsellesi notifikaatiot hintojen heilahteluista.
-Voit asentaa maksimissaan 4 sääntöä.
 
 Huom. Applen iOS laitteilla sovellus tulee "asentaa kotinäytölle", 
 jotta notificaatiot toimivat.    			
     			"""));
 
+        WebStorage.getItem("uid", uid -> {
+            if (uid == null) {
+                this.uid = UUID.randomUUID().toString();
+                requestToAllowNotifications();
+                addSubscribeListener(e -> {
+                    WebStorage.setItem("uid", this.uid);
+                    service.subscribe(this.uid, e.getSubscription());
+                    notifications = new ArrayList<>();
+                    bindData();
+                    remove(requestNotificationsBtn);
+                });
+            } else {
+                this.uid = uid;
+                notifications = service.listNotifications(uid);
+                bindData();
+            }
+        });
+
     }
 
     private void save() {
         service.saveNotifications(uid, notifications);
+        UI.getCurrent().navigate("");
     }
 
     private void bindData() {
@@ -82,6 +94,105 @@ jotta notificaatiot toimivat.
 
         add(new Button("Save", e -> save()));
         
+        add(new DeleteButton("Clear and stop notifications", () -> {
+            unsubscribe();
+        }));
+    }
+    
+    
+    public void requestToAllowNotifications() {
+        requestNotificationsBtn = new Button("Subscribe...");
+        add(requestNotificationsBtn);
+        // Notifications need to be requested from direct user interaction
+        requestNotificationsBtn.getElement().executeJs("""
+    const keyfromserver = $0;
+    const padding = '='.repeat((4 - (keyfromserver.length % 4)) % 4);
+    const base64 = (keyfromserver + padding).replace(/\\-/g, '+').replace(/_/g, '/');
+    const rawData = window.atob(base64);
+    const serverKey = new Uint8Array(rawData.length);
+    for (let i = 0; i < rawData.length; ++i) {
+        serverKey[i] = rawData.charCodeAt(i);
+    }
+
+    this.addEventListener("click", () => {
+        Notification.requestPermission().then( notificationPermission => {
+            if (notificationPermission === 'granted') {
+                navigator.serviceWorker.getRegistration().then(registration => {
+                    registration.pushManager.subscribe({
+                        userVisibleOnly: true,
+                        applicationServerKey: serverKey,
+                    }).then(subscription => {
+                        this.dispatchEvent(new CustomEvent('web-push-subscribed', {
+                            bubbles: true,
+                            composed: true,
+                            // Serialize keys uint8array -> base64
+                            detail: JSON.parse(JSON.stringify(subscription))
+                        }));
+                    }).catch( (er) => {
+                        console.error(er.message);
+                    });
+                });
+            } else {
+                window.alert("No fun then!");
+            }
+        });
+    });
+                           """, service.getPublicKey());
+
+    }
+    
+    private void unsubscribe() {
+        WebStorage.clear();
+        service.unsubscribe(uid);
+
+        getElement().executeJs("""
+navigator.serviceWorker.ready.then((reg) => {
+reg.pushManager.getSubscription().then((subscription) => {
+  subscription
+    .unsubscribe()
+    .then((successful) => {
+      // You've successfully unsubscribed
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+});
+});                               
+                               
+        """).then(r -> UI.getCurrent().getPage().reload());
+    }
+    
+        // Events
+
+    public static class WebPushSubscriptionEvent extends ComponentEvent<NotificationsView> {
+        private final Subscription subscription;
+
+        public WebPushSubscriptionEvent(NotificationsView source,
+                                        boolean fromClient,
+                                        Subscription subscription) {
+            super(source, fromClient);
+            this.subscription = subscription;
+        }
+
+        public Subscription getSubscription() {
+            return subscription;
+        }
+    }
+
+    @DomEvent("web-push-subscribed")
+    public static class SubscribeEvent extends WebPushSubscriptionEvent {
+        public SubscribeEvent(
+                NotificationsView source,
+                boolean fromClient,
+                @EventData("event.detail.endpoint") String endpoint,
+                @EventData("event.detail.keys.auth") String auth,
+                @EventData("event.detail.keys.p256dh") String p256dh) {
+            super(source, fromClient, new Subscription(endpoint, new Subscription.Keys(p256dh, auth)));
+        }
+    }
+
+    public Registration addSubscribeListener(ComponentEventListener<SubscribeEvent> listener) {
+        return addListener(SubscribeEvent.class, listener);
     }
 
 }
