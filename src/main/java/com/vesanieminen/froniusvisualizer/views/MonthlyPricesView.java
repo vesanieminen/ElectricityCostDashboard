dcad676e9aa867643616a367681b857ffd197a66
Vesa Nieminen <vesa@vaadin.com>
2023-07-07 20:53:55 +0300

    Increased the file upload size from 1MB to 5MB.

--- src/main/java/com/vesanieminen/froniusvisualizer/views/MonthlyPricesView.java
+++ src/main/java/com/vesanieminen/froniusvisualizer/views/MonthlyPricesView.java
@@ -1,16 +1,16 @@
 package com.vesanieminen.froniusvisualizer.views;
 
 import com.vaadin.flow.component.AttachEvent;
-import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.grid.GridVariant;
 import com.vaadin.flow.component.html.Main;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility;
 
-import java.util.List;
-
 import static com.vesanieminen.froniusvisualizer.services.PriceCalculatorService.calculateSpotAveragePriceOnMonth;
+import static com.vesanieminen.froniusvisualizer.services.PriceCalculatorService.spotDataEnd;
+import static com.vesanieminen.froniusvisualizer.services.PriceCalculatorService.spotDataStart;
+import static com.vesanieminen.froniusvisualizer.util.Utils.fiZoneID;
 import static com.vesanieminen.froniusvisualizer.views.MainLayout.URL_SUFFIX;
 
 @PageTitle("Monthly Prices" + URL_SUFFIX)
@@ -21,7 +21,7 @@ public class MonthlyPricesView extends Main {
     }
 
     public MonthlyPricesView() {
-        addClassNames(LumoUtility.Overflow.AUTO, LumoUtility.Padding.Horizontal.SMALL);
+        addClassNames(LumoUtility.Overflow.AUTO, LumoUtility.Padding.Horizontal.SMALL, LumoUtility.Display.FLEX, LumoUtility.FlexDirection.COLUMN);
         // Set height to correctly position sticky dates
         // Added fix for iOS Safari header height that changes when scrolling
         setHeight("var(--fullscreen-height)");
@@ -29,15 +29,20 @@ public class MonthlyPricesView extends Main {
 
     @Override
     protected void onAttach(AttachEvent attachEvent) {
+        final var startyear = spotDataStart.atZone(fiZoneID).getYear();
+        final var startMonth = spotDataStart.atZone(fiZoneID).getMonth().getValue();
+        add(new Span("Start year: " + startyear + ", start month: " + startMonth));
+
+        final var endYear = spotDataEnd.atZone(fiZoneID).getYear();
+        final var b = spotDataEnd.atZone(fiZoneID).getDayOfMonth() == spotDataEnd.atZone(fiZoneID).getDayOfMonth();
+        final var endMonth = spotDataEnd.atZone(fiZoneID).getMonth().getValue() - 1;
+        add(new Span("End year: " + endYear + ", end month: " + endMonth));
+
+        //final var timeRangeSpan = new Span(format(spotDataStart, getLocale()) + " - " + format(spotDataEnd, getLocale()));
+
         final var v = calculateSpotAveragePriceOnMonth(2021, 1);
-        final var monthDataGrid = new Grid<MonthData>();
-        monthDataGrid.addThemeVariants(GridVariant.LUMO_NO_BORDER);
-        monthDataGrid.setHeightFull();
-        monthDataGrid.addColumn(MonthData::month);
-        monthDataGrid.addColumn(MonthData::averagePrice);
-        final var monthDataList = List.of(new MonthData(2021, 1, v));
-        monthDataGrid.setItems(monthDataList);
-        add(monthDataGrid);
+
+
     }
 
 }
