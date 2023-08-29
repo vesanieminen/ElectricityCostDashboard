3b2781c53787fc3b5cc23eef0d898ec13eed589e
Vesa Nieminen <vesa@vaadin.com>
2023-08-29 13:44:51 +0300

    Hide the range selector group.

--- src/main/java/com/vesanieminen/froniusvisualizer/views/ChartTemplateViewForTimo.java
+++ src/main/java/com/vesanieminen/froniusvisualizer/views/ChartTemplateViewForTimo.java
@@ -10,7 +10,10 @@ import com.vesanieminen.froniusvisualizer.components.BarChartTemplateTimo;
 import com.vesanieminen.froniusvisualizer.components.DoubleLabel;
 
 import java.text.NumberFormat;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
+import static com.vesanieminen.froniusvisualizer.services.NordpoolSpotService.getLatest7Days;
 import static com.vesanieminen.froniusvisualizer.services.PriceCalculatorService.calculateSpotAveragePriceThisMonth;
 import static com.vesanieminen.froniusvisualizer.services.PriceCalculatorService.calculateSpotAveragePriceToday;
 import static com.vesanieminen.froniusvisualizer.util.Utils.getNumberFormat;
@@ -23,20 +26,36 @@ public class ChartTemplateViewForTimo extends Main {
     public ChartTemplateViewForTimo() {
         //setHeight("var(--fullscreen-height)");
         //setMinHeight("300px");
-        final var h2 = new H2("Liukuri.fi");
-        h2.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.TextAlignment.CENTER, LumoUtility.Margin.Top.SMALL);
-        add(h2);
-        add(new BarChartTemplateTimo());
 
+        final var latest7Days = getLatest7Days();
+        final var latestData = latest7Days.data.DataEnddate.minusDays(1);
+        final var day = DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG).withLocale(getLocale()).format(latestData);
+
+        final var span = new H2(" - Liukuri.fi");
+        span.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.TextColor.SECONDARY);
+        final var h2 = new H2("%s".formatted(day));
+        h2.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.TextAlignment.CENTER);
+        final var title = new Div(h2, span);
+        title.addClassNames(LumoUtility.Display.FLEX, LumoUtility.JustifyContent.CENTER, LumoUtility.Margin.Top.SMALL, LumoUtility.Gap.SMALL);
+        add(title);
+
+        add(new BarChartTemplateTimo());
 
         final NumberFormat numberFormat = getNumberFormat(getLocale(), 2);
         numberFormat.setMinimumFractionDigits(2);
         final var averageTodayLabel = new DoubleLabel(getTranslation("Average today"), numberFormat.format(calculateSpotAveragePriceToday()) + " " + getTranslation("c/kWh"));
         final var averageThisMonthLabel = new DoubleLabel(getTranslation("Average this month"), numberFormat.format(calculateSpotAveragePriceThisMonth()) + " " + getTranslation("c/kWh"));
+        averageThisMonthLabel.getSpanBottom().getStyle().set("border-color", "rgb(242, 182, 50)");
+        averageThisMonthLabel.getSpanBottom().getStyle().set("border-width", "medium");
+        averageThisMonthLabel.getSpanBottom().addClassNames(LumoUtility.Border.BOTTOM);
 
         final var div = new Div(averageTodayLabel, averageThisMonthLabel);
         div.addClassNames(LumoUtility.Display.FLEX, LumoUtility.FlexWrap.WRAP, LumoUtility.Width.FULL/*, LumoUtility.BorderRadius.LARGE, LumoUtility.Border.ALL, LumoUtility.BorderColor.CONTRAST_10*/);
         add(div);
+    }
+
+    private DoubleLabel getLowestAndHighestPriceToday() {
+        return null;
     }
 
 }
