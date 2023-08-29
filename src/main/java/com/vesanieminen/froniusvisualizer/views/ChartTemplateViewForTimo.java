e3025f711341a3878f7c5ad14a5a741e1ba419ff
Vesa Nieminen <vesa@vaadin.com>
2023-08-29 20:48:54 +0300

    Changed the title size.

--- src/main/java/com/vesanieminen/froniusvisualizer/views/ChartTemplateViewForTimo.java
+++ src/main/java/com/vesanieminen/froniusvisualizer/views/ChartTemplateViewForTimo.java
@@ -5,6 +5,7 @@ import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Main;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
 import com.vaadin.flow.theme.lumo.LumoUtility;
 import com.vesanieminen.froniusvisualizer.components.BarChartTemplateTimo;
 import com.vesanieminen.froniusvisualizer.components.DoubleLabel;
@@ -14,12 +15,14 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
 import static com.vesanieminen.froniusvisualizer.services.NordpoolSpotService.getLatest7Days;
+import static com.vesanieminen.froniusvisualizer.services.NordpoolSpotService.getLatest7DaysList;
 import static com.vesanieminen.froniusvisualizer.services.PriceCalculatorService.calculateSpotAveragePriceThisMonth;
 import static com.vesanieminen.froniusvisualizer.services.PriceCalculatorService.calculateSpotAveragePriceToday;
 import static com.vesanieminen.froniusvisualizer.util.Utils.getNumberFormat;
 import static com.vesanieminen.froniusvisualizer.views.MainLayout.URL_SUFFIX;
 
-@Route(value = "pörssisähköryhmä", layout = MainLayout.class)
+@Route(value = "porssisahkoryhma", layout = MainLayout.class)
+@RouteAlias(value = "pörssisähköryhmä", layout = MainLayout.class)
 @PageTitle("Market electricity group" + URL_SUFFIX)
 public class ChartTemplateViewForTimo extends Main {
 
@@ -32,9 +35,9 @@ public class ChartTemplateViewForTimo extends Main {
         final var day = DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG).withLocale(getLocale()).format(latestData);
 
         final var span = new H2(" - Liukuri.fi");
-        span.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.TextColor.SECONDARY);
+        span.addClassNames(LumoUtility.FontSize.MEDIUM, LumoUtility.TextColor.SECONDARY, LumoUtility.Display.FLEX, LumoUtility.AlignItems.CENTER);
         final var h2 = new H2("%s".formatted(day));
-        h2.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.TextAlignment.CENTER);
+        h2.addClassNames(LumoUtility.FontSize.LARGE);
         final var title = new Div(h2, span);
         title.addClassNames(LumoUtility.Display.FLEX, LumoUtility.JustifyContent.CENTER, LumoUtility.Margin.Top.SMALL, LumoUtility.Gap.SMALL);
         add(title);
@@ -43,6 +46,7 @@ public class ChartTemplateViewForTimo extends Main {
 
         final NumberFormat numberFormat = getNumberFormat(getLocale(), 2);
         numberFormat.setMinimumFractionDigits(2);
+        final var latest7DaysList = getLatest7DaysList();
         final var averageTodayLabel = new DoubleLabel(getTranslation("Average today"), numberFormat.format(calculateSpotAveragePriceToday()) + " " + getTranslation("c/kWh"));
         final var averageThisMonthLabel = new DoubleLabel(getTranslation("Average this month"), numberFormat.format(calculateSpotAveragePriceThisMonth()) + " " + getTranslation("c/kWh"));
         averageThisMonthLabel.getSpanBottom().getStyle().set("border-color", "rgb(242, 182, 50)");
