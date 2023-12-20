6dd8d178cc68965cb89c4e5b4bbb9f1fbaabfde9
Vesa Nieminen <vesa@vaadin.com>
2023-12-20 16:31:57 +0200

    Migrated another pojo from Java to Kotlin.

--- src/main/java/com/vesanieminen/froniusvisualizer/services/model/TVOResponse.kt
+++ src/main/java/com/vesanieminen/froniusvisualizer/services/model/TVOResponse.kt
@@ -1,15 +1,11 @@
-package com.vesanieminen.froniusvisualizer.services.model;
+package com.vesanieminen.froniusvisualizer.services.model
 
-import java.util.List;
+class TVOResponse {
+    var created: String? = null
+    var predictions: List<Data>? = null
 
-public class TVOResponse {
-
-    public String created;
-    public List<Data> predictions;
-
-    public static class Data {
-        public String time;
-        public int prediction;
+    class Data {
+        var time: String? = null
+        var prediction = 0
     }
-
 }
