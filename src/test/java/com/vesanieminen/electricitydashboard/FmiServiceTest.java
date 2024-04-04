d1cfc9f7ba12a889ef00cc54cb894990fe6b8c74
Vesa Nieminen <vesa.nieminen@gmail.com>
2024-04-04 14:56:27 +0300

    Temporarily removed the FMIService from use due to an NPE exception.

--- src/test/java/com/vesanieminen/electricitydashboard/FmiServiceTest.java
+++ src/test/java/com/vesanieminen/electricitydashboard/FmiServiceTest.java
@@ -1,6 +1,5 @@
 package com.vesanieminen.electricitydashboard;
 
-import com.vesanieminen.froniusvisualizer.services.FmiService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -8,11 +7,11 @@ import org.junit.jupiter.api.Test;
 public class FmiServiceTest {
     @Test
     public void testFmi() {
-        var observations = FmiService.getObservations().getObservations();
+        //var observations = FmiService.getObservations().getObservations();
 
-        for (var observation : observations) {
-            System.out.println(observation.getName());
-            System.out.println(FmiService.parseFmiTimestamp(observation.getLocaltime(), observation.getLocaltz()));
-        }
+        //for (var observation : observations) {
+        //    System.out.println(observation.getName());
+        //    System.out.println(FmiService.parseFmiTimestamp(observation.getLocaltime(), observation.getLocaltz()));
+        //}
     }
 }
