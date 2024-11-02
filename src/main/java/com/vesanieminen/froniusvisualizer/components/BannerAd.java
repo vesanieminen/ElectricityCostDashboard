509863faeb8341a86c38e13d4fa2b8f499ee548d
Vesa Nieminen <vesa.nieminen@gmail.com>
2024-11-02 15:57:32 +0200

    Create banner ad for the YouTube stream.

---
+++ src/main/java/com/vesanieminen/froniusvisualizer/components/BannerAd.java
@@ -0,0 +1,65 @@
+package com.vesanieminen.froniusvisualizer.components;
+
+import com.vaadin.flow.component.Unit;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+
+public class BannerAd extends Div {
+
+    public BannerAd() {
+        addClassNames(
+                LumoUtility.Margin.Vertical.NONE
+        );
+
+        setWidthFull();
+
+        // Create text components
+        Span title = new Span(getTranslation("liukuri.live.pks.interview"));
+        title.addClassNames(
+                LumoUtility.FontWeight.BOLD,
+                LumoUtility.FontSize.MEDIUM
+                //LumoUtility.Whitespace.NOWRAP,
+                //LumoUtility.Overflow.HIDDEN,
+                //LumoUtility.TextOverflow.ELLIPSIS
+        );
+        Span date = new Span(getTranslation("liukuri.live.pks.interview.datetime"));
+        date.addClassNames(
+                LumoUtility.TextColor.SECONDARY
+        );
+
+        // Arrange text vertically
+        VerticalLayout textLayout = new VerticalLayout(title, date);
+        textLayout.setSpacing(false);
+        textLayout.setPadding(false);
+        //textLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+        //textLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+
+        final var youTubeImage = new Image("images/YouTube_full-color_icon_(2017).svg.png", "YouTube");
+        youTubeImage.setMaxHeight(30, Unit.PIXELS);
+        final var horizontalLayout = new HorizontalLayout(textLayout, youTubeImage);
+        horizontalLayout.addClassNames(
+                LumoUtility.AlignItems.CENTER,
+                LumoUtility.Gap.MEDIUM
+        );
+        horizontalLayout.setPadding(false);
+        horizontalLayout.setSpacing(false);
+
+        final var parent = new HorizontalLayout(horizontalLayout);
+        parent.addClassNames(
+                LumoUtility.JustifyContent.CENTER
+        );
+
+        // Make the banner clickable
+        Anchor bannerLink = new Anchor("https://youtube.com/live/BPAdJQIYFiU", parent);
+        bannerLink.setTarget("_blank"); // Open in new tab
+
+        // Center the content
+        add(bannerLink);
+    }
+
+}
