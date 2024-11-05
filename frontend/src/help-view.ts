c2a9378a6c9e6cfd83ba553bdf219ca9c806fcdf
Vesa Nieminen <vesa.nieminen@gmail.com>
2024-11-05 21:35:15 +0200

    Small changes.

--- frontend/src/help-view.ts
+++ frontend/src/help-view.ts
@@ -46,7 +46,7 @@ export class HelpView extends LitElement {
                             src="https://www.youtube.com/embed/MmN86VN0vYY?si=t_lB6lMkg17budxb"
                             title="YouTube video player" frameborder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            allowfullscreen>
+                            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
                     </iframe>
 
                 </div>
